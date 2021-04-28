b18d55eebd5a930eda29537d545e30d9545290c0
Eduardo Costa Rodrigues <edu.crod@gmail.com>
2021-04-27 23:43:10 -0300

    Add project files.

---
+++ ElephantExercise/Elephant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElephantExercise
+{
+    class Elephant
+    {
+        public int EarSize;
+        public string Name;
+
+        public void WhoAmI()
+        {
+            Console.WriteLine($"My name is {Name}.");
+            Console.WriteLine($"My ears are {EarSize} inches tall.");
+        }
+
+    }
+}
---
+++ ElephantExercise/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ElephantExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elephant lucinda = new Elephant() { Name = "Lucinda", EarSize = 33 };
+            Elephant lloyd = new Elephant() { Name = "Lloyd", EarSize = 40 };
+                      
+            while (true)
+            {
+                
+                Console.WriteLine("Press 1 for Lloyd, 2 for Lucinda, 3 to swap");
+                
+                string line = Console.ReadLine();
+                
+                if (int.TryParse(line, out int value))
+                {
+                    if (value == 1)
+                    {
+                        Console.WriteLine("You pressed 1");
+                        Console.WriteLine("Calling lloyd.WhoAmI()");
+                        lloyd.WhoAmI();
+
+                    } else if (value == 2 )
+                    {
+                        Console.WriteLine("You pressed 1");
+                        Console.WriteLine("Calling lucinda.WhoAmI()");
+                        lucinda.WhoAmI();
+                    }else if (value == 3)
+                    {
+                        Console.WriteLine("You pressed 3");
+                        Elephant swap;
+                        swap = lloyd;
+                        lloyd = lucinda;
+                        lucinda = swap;
+                        
+                        Console.WriteLine("References have been swapped");
+                    }
+
+                }
+            }
+        }
+    }
+}
